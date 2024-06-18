28624a35f07a095edc6a0abfea9cedc2deef043a
saumitraebits <saumitra@ealphabits.com>
2024-06-18 18:42:50 +0530

    Added tailwind classes as an example in Home.vue and modified README.md

--- components/Home.vue
+++ components/Home.vue
@@ -1,13 +1,7 @@
 <template>
-    <div>
-        Hello World
-    </div>
+  <h1 class="text-4xl font-semibold text-slate-900 text-center w-full mt-10">Hello World</h1>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
